97d57797120b10bf2cbea0f1ad03b2c190a0e6fb
bruno_rafaeli <brunorafaeli@tgr.net.br>
2017-11-30 15:05:57 -0200

    Fixing modal

--- cg/js/main.js
+++ cg/js/main.js
@@ -480,22 +480,25 @@ window.onload = function() {
       }
   });
 
-  function reposition() {
-      var modal = $(this),
-      dialog = modal.find('.modal-dialog');
-      modal.css('display', 'block');
-      // Dividing by two centers the modal exactly, but dividing by three
-      // or four works better for larger screens.
-      dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
-  }
-  // Reposition when a modal is shown
-  $('.modal').on('show.bs.modal', reposition);
-  // Reposition when the window is resized
-  $(window).on('resize', function() {
-      $('.modal:visible').each(reposition);
-  });
+  // Get the modal
+  var modal = document.getElementById('myModal');
 
-  $('#myModal').modal('show');
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  modal.style.display = "block";
 
 };
 
